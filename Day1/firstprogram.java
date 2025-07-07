456355b044160264286b1bf1a90a1d3d5a41a453
Info-God <karanvel.2005@gmail.com>
2025-07-07 07:28:08 +0530

    Day 1

---
+++ Day1/firstprogram.java
@@ -0,0 +1,29 @@
+class firstprogram {
+    public static void main(String args[]){
+        //basics of java
+        System.out.println("coming broo");
+
+        int age = 10;
+
+        int revisedAge = age + 5;
+
+        int num1 = 10;
+
+        int num2 = 20;
+
+        int num3 = num1;
+
+        num1 = num2;
+
+        num2 = num3;
+
+        float value = 10.3f;
+
+        int val = (int) value;
+
+
+
+        System.out.println(val);
+        System.out.println(num2);
+    }
+}
---
+++ Day1/strings.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+class strings {
+    public static void main(String args[]){
+        // String name = "karan";
+
+        // //print name
+        // System.out.println(name);
+
+        // //get the character
+        // System.out.println(name.charAt(2));
+
+        // // get range of values
+
+        // System.out.println(name.substring(0,2));
+
+        Scanner inputData = new Scanner(System.in);
+        // System.out.println("Enter your name");
+
+        // String name = inputData.nextLine();
+
+        // System.out.println("Enter your age");
+
+        // short age = inputData.nextShort();
+
+        // System.out.println("name" + name + "age" + age);
+
+        // StringBuilder name1 = new StringBuilder(name);
+
+        // System.out.println(name1.reverse());
+        // System.out.println("Enter number of subjects");
+        
+        // short lengthOfSubject = inputData.nextShort();
+
+        // short[] marks = new short[lengthOfSubject];
+
+        // for(int i=0;i<lengthOfSubject;i++){
+        //     System.out.println("Enter mark of " + i + " Subject :");
+
+        //     marks[i] = inputData.nextShort();
+
+        //     // int total += marks[i-1];
+        // }
+
+        // System.out.println("Marks are ");
+
+        // for (int n : marks){
+        //     System.out.println(n);
+        // }
+
+
+        // int len = 10;
+
+        // for(int i=1;i<=10;i++){
+        //     if(i % 2 == 0){
+        //         System.out.println(i);
+        //     }
+
+        // }
+        // int j = 1;
+        // while(j<=len){
+        //     System.out.println(j);
+        //     j++;
+        // }
+
+
+
+        System.out.println()
+
+        
+
+
+        
+        
+
+
+        inputData.close();
+    }
+}
